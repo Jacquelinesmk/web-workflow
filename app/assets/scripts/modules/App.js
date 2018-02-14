990a2f6aed1f78a0e36269f38195bfe0971dcfdf
Christian Maekawa <christianmaekawa@unifei.edu.br>
2018-02-14 10:52:31 -0200

    Completed the review on scroll behavior.

--- app/assets/scripts/modules/App.js
+++ app/assets/scripts/modules/App.js
@@ -1,3 +1,6 @@
 import MobileMenu from "./MobileMenu";
-
+import RevealOnScroll from "./RevealOnScroll";
+import $ from 'jquery';
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"),"85%");
+new RevealOnScroll($(".testimonial"),"60%");

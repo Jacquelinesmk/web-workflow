2099180c89f50091d7722710af157ccbe0defbcd
Christian Maekawa <christianmaekawa@unifei.edu.br>
2018-02-14 14:42:20 -0200

    Completed the modal overlay.

--- app/assets/scripts/modules/App.js
+++ app/assets/scripts/modules/App.js
@@ -1,8 +1,10 @@
 import MobileMenu from "./MobileMenu";
 import RevealOnScroll from "./RevealOnScroll";
+import Modal from './Modal';
 import $ from 'jquery';
 import StickyHeader from './StickyHeader';
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"),"60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
